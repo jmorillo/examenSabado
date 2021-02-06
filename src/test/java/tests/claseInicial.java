94f9e57e1f53307945d336686782c3ebd7a6bf97
CORP\juan.morillo <juanmorillosicilia@gmail.com>
2021-02-06 17:56:04 +0100

    actualizando

--- src/test/java/tests/claseInicial.java
+++ src/test/java/tests/claseInicial.java
@@ -6,17 +6,22 @@ import org.testng.annotations.Test;
 import utilities.GetProperties;
 
 public class claseInicial {
+    private WebDriver driver;
+
     @Test
-    public void ejercicio1(){
+    public void inicializador(){
         GetProperties properties = new GetProperties();
         String chromeURL = properties.getString("CHROMEDRIVER_PATH");
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\juan.morillo\\Desktop\\programacion\\clase9\\drivers\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", chromeURL);
+        driver = new ChromeDriver();
+        //driver.get("http://www.google.com");
+    }
 
-        WebDriver driver = new ChromeDriver();
-
+    @Test
+    public void ejercicio1(){
+        inicializador();
         driver.get("http://www.google.com");
-
     }
 
 }
