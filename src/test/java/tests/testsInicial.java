6ab1842a369a13290f7242d1f7ba16be1037869e
CORP\juan.morillo <juanmorillosicilia@gmail.com>
2021-02-06 17:54:23 +0100

    cambiando

--- src/test/java/tests/testsInicial.java
+++ src/test/java/tests/testsInicial.java
@@ -6,17 +6,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import utilities.GetProperties;
 
 public class testsInicial {
+
+    private WebDriver driver;
     @Test
-    public void ejercicio1(){
+    public void inicializador(){
         GetProperties properties = new GetProperties();
         String chromeURL = properties.getString("CHROMEDRIVER_PATH");
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\juan.morillo\\Desktop\\Proyecto\\Selenium\\driver\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", chromeURL);
+        driver = new ChromeDriver();
+       // driver.get("http://www.google.com");
+    }
 
-        WebDriver driver = new ChromeDriver();
-
+    @Test
+    public void ejercicio1(){
+        inicializador();
         driver.get("http://www.google.com");
-
     }
 
 }
