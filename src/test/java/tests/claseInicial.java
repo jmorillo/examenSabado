7acfc391cd74704023ddf410b5d0da4c4fbe4d3f
CORP\juan.morillo <juanmorillosicilia@gmail.com>
2021-02-06 18:12:56 +0100

    primeros cambios

--- src/test/java/tests/claseInicial.java
+++ src/test/java/tests/claseInicial.java
@@ -1,27 +1,31 @@
 package tests;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.testng.thread.IThreadWorkerFactory;
 import utilities.GetProperties;
 
 public class claseInicial {
     private WebDriver driver;
 
     @Test
-    public void inicializador(){
+    public void inicializador() throws InterruptedException {
         GetProperties properties = new GetProperties();
         String chromeURL = properties.getString("CHROMEDRIVER_PATH");
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\juan.morillo\\Desktop\\programacion\\clase9\\drivers\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", chromeURL);
         driver = new ChromeDriver();
-        //driver.get("http://www.google.com");
+        driver.get("https://www.netflix.com/");
+        driver.manage().window().setSize(new Dimension(1980, 1200));
+        //esperamos
+        Thread.sleep(2500);
     }
 
     @Test
-    public void ejercicio1(){
-        inicializador();
-        driver.get("http://www.google.com");
+    public void validarTituloTest() {
+                
     }
+    
 
 }
